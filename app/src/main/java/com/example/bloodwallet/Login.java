a6999a0ebb82d63257d87af6bf2aa7d2be3c7949
tkdgus1030 <63357094+tkdgus1030@users.noreply.github.com>
2020-05-31 15:20:57 +0900

    [UPDATE] misc.xml

--- app/src/main/java/com/example/bloodwallet/Login.java
+++ app/src/main/java/com/example/bloodwallet/Login.java
@@ -1,14 +1,28 @@
 package com.example.bloodwallet;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+
+public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String TAG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +32,50 @@ public class Login extends AppCompatActivity {
         Button d = findViewById(R.id.registerbutton_login);
         d.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent i = new Intent(  Login.this , Register.class );
+                Intent i = new Intent(Login.this, Register.class);
                 startActivity(i);
             }
         });
-            Button LoginIntoMainInfo = findViewById(R.id.loginbutton_login);
-            LoginIntoMainInfo.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent i = new Intent(Login.this, MainInfo.class);
-                    startActivity(i);
-                }
-            });
-        }
+        Button LoginIntoMainInfo = findViewById(R.id.loginbutton_login);
+        LoginIntoMainInfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, MainInfo.class);
+                startActivity(i);
+            }
+        });
+        TextView ID = (TextView) findViewById(R.id.login_id);
+        TextView PW = (TextView) findViewById(R.id.login_password);
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(ID.getText().toString(), PW.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+}
 
