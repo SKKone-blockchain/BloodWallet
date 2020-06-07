1ee97daabde2efab9da4af298a6a80a02ea1de90
Yun <58709201+pleiades-s@users.noreply.github.com>
2020-06-07 17:09:32 +0900

    Update payer.js

--- fee-payer/routes/payer.js
+++ fee-payer/routes/payer.js
@@ -10,21 +10,14 @@ runFeePayer = (reqBody, res) => {
     senderRawTransaction: reqBody.senderRawTX,
     feePayer: feePayer.address
   }).on('transactionHash', function (hash) {
-    // ====================================================
-    // TX hash of TX payer has sent to Klaytn
-    // ====================================================
+
 
     // Notify the client
     res.json({ txhash: hash });
   }).on('receipt', function (receipt) {
-    // ====================================================
-    // TX has been successfully included in a recent block
-    // ====================================================
 
-    // TODO record this event
   }).on('error', function (err, receipt) {
     // Something bad happened
-    // In case of an out-of-gas error, the second parameter is the receipt containing the transaction caused the error.
     res.json({
       txhash: '',
       error: err.message
